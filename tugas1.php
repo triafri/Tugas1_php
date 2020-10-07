7a289514f204c145d8ecb4ab9de35ce0354fe53c
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-10-07 10:16:52 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,36 @@
+<?php
+$menu1="Ayam Goreng";
+$menu2="Ayam Bakar";
+$menu3="Ayam Sayur";
+$menu4="Tempe Goreng";
+$menu5="Tahu Goreng";
+$menu6="Soto";
+$menu7="Nasi Putih";
+$menu8="Es Teh";
+$menu9="Es Tebu";
+
+$harga1=13000;
+$harga2=15000;
+$harga3=13000;
+$harga4=1000;
+$harga5=1000;
+$harga6=3000;
+$harga7=5000;
+$harga8=4000;
+$harga9=5000;
+
+echo"<h1>Daftar Menu</h1>";
+echo"<ul>
+		<li>".$menu1." = ".$harga1."</li>
+		<li>".$menu2." = ".$harga2."</li>
+		<li>".$menu3." = ".$harga3."</li>
+		<li>".$menu4." = ".$harga4."</li>
+		<li>".$menu5." = ".$harga5."</li>
+		<li>".$menu6." = ".$harga6."</li>
+		<li>".$menu7." = ".$harga7."</li>
+		<li>".$menu8." = ".$harga8."</li>
+		<li>".$menu9." = ".$harga9."</li>
+	</ul>";
+
+
+?>
